959a7d112a777846846b452ea165c86ed1fcd430
BhagyaDidulanka <didulankabhagya@gmail.com>
2025-04-22 15:47:58 +0530

    set router

--- mern-blog-app/backend/server.js
+++ mern-blog-app/backend/server.js
@@ -1,4 +1,4 @@
-require('ditenv').config();
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use('/api/posts',postRoutes);
 
 //MongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB '))
     .catch(err => console.log('MongoDB connection error:',err));
 
