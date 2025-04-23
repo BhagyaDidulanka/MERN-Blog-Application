f17a7a4c041da4dcd22c35615b57a3eff3326c59
BhagyaDidulanka <didulankabhagya@gmail.com>
2025-04-23 11:41:49 +0530

    last comment

--- mern-blog-app/frontend/src/App.js
+++ mern-blog-app/frontend/src/App.js
@@ -1,12 +1,26 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CreatePost from './pages/CreatePost';
+import PostDetail from './pages/PostDetail';
+import EditPost from './pages/EditPost';
+import NotFound from './pages/NotFound';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header >
-        
-      </header>
-    </div>
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Header/>
+          <Routes>
+          <Route path="/" element={<Home />} />
+            <Route path="/posts/create" element={<CreatePost />} />
+            <Route path="/posts/:id" element={<PostDetail />} />
+            <Route path="/posts/:id/edit" element={<EditPost />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+      </div>
+    </Router>
   );
 }
 
